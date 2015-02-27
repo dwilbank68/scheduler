ec1fd75884158a0b79b65e8171ce62da48619072
David Wilbanks <dwilbank@gmail.com>
2015-02-27 12:42:37 -0800

    improved handlebars template fidelity

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -51,7 +51,12 @@ $(document).ready(function() {
         }
     });
 
-
+    Handlebars.registerHelper('if_eq', function(a, b, opts) {
+        if(a == b) // Or === depending on your needs
+            return opts.fn(this);
+        else
+            return opts.inverse(this);
+    });
 
 
 
