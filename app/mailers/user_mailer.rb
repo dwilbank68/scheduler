45a05e12be0458b461f930071ff167997b0afce8
David Wilbanks <dwilbank@gmail.com>
2015-03-25 09:27:26 -0700

    Update user_mailer.rb

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,12 +1,10 @@
 class UserMailer < ActionMailer::Base
   default from: "scheduler-wilbanks@herokuapp.com"
 
-  # def send_email(user, msg)
   def send_email(user, email, msg)
     @user = user
     @msg = msg
     mail( :to => email, :subject => 'Hello from Scheduler' )
-    # mail( :to => @user.email, :subject => 'Hello from Scheduler' ) if @user.contact_flags[3] == '1'
   end
 
 end
